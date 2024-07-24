666b14d235023b0d60ba2e52d27b5dab16afcc5c
username <wecp-dummy@gmail.com>
2024-07-24 13:02:41 +0000

    Day 17 done

--- frontend/src/app/bank/components/account/account.component.ts
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -1,5 +1,9 @@
+import { AccountTS } from "../../types/tstypes/Accountts";
 
 export class AccountComponent {
- 
-
+    account:any=AccountTS;
+    constructor(){
+        this.account=new AccountTS("1",1000.00,"1");
+    }
+    
 }
--- frontend/src/app/bank/components/customer/customer.component.ts
+++ frontend/src/app/bank/components/customer/customer.component.ts
@@ -1,5 +1,8 @@
+import { CustomerTS } from "../../types/tstypes/Customerts";
 
 export class CustomerComponent  {
-
-  
+    customer:any=CustomerTS;
+    constructor(){
+        this.customer=new CustomerTS("John Doe", "john@example.com", "john_doe", "password123", "User","1");
+    }  
 }
